f58fc68855a909f5add1ae931c1456cee105b6e6
unknown <manikanta.official1408@gmail.com>
2023-09-14 16:07:38 +0530

    COmmitted

---
+++ Data/MovieDbContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Api_For_Movies.Model;
+
+namespace Api_For_Movies.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        public MovieDbContext (DbContextOptions<MovieDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Api_For_Movies.Model.Movie> Movie { get; set; } = default!;
+    }
+}
